44f8ab6122be25710a6dcbc9d0743caf2bafdd35
Waldemar <waldemar.jankowski95@gmail.com>
2024-09-12 16:50:10 +0200

    Add RequestValidationException and ResourceNotFoundException

---
+++ src/main/java/com/wj/shared/definition/RequestValidationException.java
@@ -0,0 +1,21 @@
+package com.wj.shared.definition;
+
+public class RequestValidationException extends RuntimeException {
+
+    private static final String ALREADY_EXISTS_MESSAGE_TEMPLATE = "%s already exists (%s: %s)";
+    private static final String NOT_FOUND_MESSAGE_TEMPLATE = "%s does not exist (%s: %s)";
+
+    public RequestValidationException(String message) {
+        super(message);
+    }
+
+    public static RequestValidationException alreadyExists(String entityName, String fieldName, String fieldValue) {
+        String message = String.format(ALREADY_EXISTS_MESSAGE_TEMPLATE, entityName, fieldName, fieldValue);
+        return new RequestValidationException(message);
+    }
+
+    public static RequestValidationException notFound(String entityName, String fieldName, long fieldValue) {
+        String message = String.format(NOT_FOUND_MESSAGE_TEMPLATE, entityName, fieldName, fieldValue);
+        return new RequestValidationException(message);
+    }
+}
---
+++ src/main/java/com/wj/shared/definition/ResourceNotFoundException.java
@@ -0,0 +1,15 @@
+package com.wj.shared.definition;
+
+public class ResourceNotFoundException extends RuntimeException {
+
+    private static final String NOT_FOUND_MESSAGE_TEMPLATE = "%s does not exist (%s: %s)";
+
+    public ResourceNotFoundException(String message) {
+        super(message);
+    }
+
+    public static ResourceNotFoundException notFound(String entityName, String fieldName, long fieldValue) {
+        String message = String.format(NOT_FOUND_MESSAGE_TEMPLATE, entityName, fieldName, fieldValue);
+        return new ResourceNotFoundException(message);
+    }
+}
